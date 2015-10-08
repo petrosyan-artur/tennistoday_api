3fd17c80fc2acfd162cfc99aeca64cc544b424e0
Aram Knyazyan <aramknyazyan@gmail.com>
2015-10-08 18:55:13 +0400

    --git add

---
+++ src/TTMainBundle/Entity/SubCompanies.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace TTMainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+/**
+ * SubCompanies
+ *
+ * @ORM\Table(name="sub_companies")
+ * @ORM\Entity(repositoryClass="TTMainBundle\Entity\Repository\SubCompaniesRepository")
+ */
+class SubCompanies
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="company_name", type="string", length=50, nullable=true, unique=true)
+     */
+    private $companyName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="company_sub_name", type="string", length=50, nullable=true, unique=true)
+     */
+    private $companySubName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country_iso_code", type="string", length=2, nullable=true)
+     */
+    private $countryIsoCode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="state", type="string", length=50, nullable=true)
+     */
+    private $state;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=50, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street", type="string", length=50, nullable=true)
+     */
+    private $street;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone_number", type="string", length=50, nullable=true)
+     */
+    private $phoneNumber;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set companyName
+     *
+     * @param string $companyName
+     *
+     * @return SubCompanies
+     */
+    public function setCompanyName($companyName)
+    {
+        $this->companyName = $companyName;
+
+        return $this;
+    }
+
+    /**
+     * Get companyName
+     *
+     * @return string
+     */
+    public function getCompanyName()
+    {
+        return $this->companyName;
+    }
+
+    /**
+     * Set companySubName
+     *
+     * @param string $companySubName
+     *
+     * @return SubCompanies
+     */
+    public function setCompanySubName($companySubName)
+    {
+        $this->companySubName = $companySubName;
+
+        return $this;
+    }
+
+    /**
+     * Get companySubName
+     *
+     * @return string
+     */
+    public function getCompanySubName()
+    {
+        return $this->companySubName;
+    }
+
+    /**
+     * Set countryIsoCode
+     *
+     * @param string $countryIsoCode
+     *
+     * @return SubCompanies
+     */
+    public function setCountryIsoCode($countryIsoCode)
+    {
+        $this->countryIsoCode = $countryIsoCode;
+
+        return $this;
+    }
+
+    /**
+     * Get countryIsoCode
+     *
+     * @return string
+     */
+    public function getCountryIsoCode()
+    {
+        return $this->countryIsoCode;
+    }
+
+    /**
+     * Set state
+     *
+     * @param string $state
+     *
+     * @return SubCompanies
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * Get state
+     *
+     * @return string
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     *
+     * @return SubCompanies
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set street
+     *
+     * @param string $street
+     *
+     * @return SubCompanies
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    /**
+     * Get street
+     *
+     * @return string
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set phoneNumber
+     *
+     * @param string $phoneNumber
+     *
+     * @return SubCompanies
+     */
+    public function setCoverType($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get phoneNumber
+     *
+     * @return string
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+}
